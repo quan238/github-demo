964b6c7d189274f7c828b0b9e4832f31cb0c6cf0
quan238 <quantran2381@gmail.com>
2023-06-07 15:51:17 +0700

    create data user

---
+++ index.js
@@ -0,0 +1,7 @@
+const user = [
+    { id: 1, name: 'John Doe', email: '' },
+    { id: 2, name: 'Jane Doe', email: '' },
+    { id: 3, name: 'John Smith', email: '' },
+    { id: 4, name: 'Jane Smith', email: '' },
+    { id: 5, name: 'John Doe', email: '' },
+]
